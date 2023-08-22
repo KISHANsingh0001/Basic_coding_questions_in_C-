7361e9044c74a7263e0441d324a730c1596dbd8a
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-08-22 19:00:05 +0530

    dsa practiec

--- pro21.cpp
+++ pro21.cpp
@@ -1,42 +1,28 @@
-//       Best time to buy ans sell stocks problem  [Leetcode problem number 121] by recursion and iteratively both
+//                     Add Sum problem    input -->  38   3 + 8 = 11             leetcode problem
+//                                        output --> 2    1 + 1 = 2
 #include <iostream>
-#include <vector>
-#include <limits.h>
 using namespace std;
-void buysellStocksgenrator(vector<int> &prices, int i, int &miniprice, int &maxprofit)
+int addSum(int &num)
 {
-    // base case
-    if (i == prices.size())
+    if (num == 0)
     {
-        return;
+        return 0;
     }
-
-    if (prices[i] < miniprice)
+    if (num % 9 == 0)
     {
-        miniprice = prices[i];
+        return 9;
     }
-    if ((prices[i] - miniprice) > maxprofit)
+    else
     {
-        maxprofit = (prices[i] - miniprice);
+        return num % 9;
     }
-    buysellStocksgenrator(prices, i + 1, miniprice, maxprofit);
 }
 int main()
 {
-    vector<int> prices{1, 2, 3, 4};
-    int miniprice = INT_MAX;
-    int maxprofit = INT_MIN;
-    buysellStocksgenrator(prices, 0, miniprice, maxprofit);
-    //  int miniprice = INT_MAX;
-    //     int maxprofit = INT_MIN;
-    //     for(int i = 0; i<prices.size() ; i++){
-    //         if(prices[i] < miniprice){
-    //             miniprice = prices[i];
-    //         }
-    //         if((prices[i]-miniprice)> maxprofit){
-    //             maxprofit = prices[i] - miniprice;
-    //         }
-    //     }
-    cout << " Max profit we gained is : " << maxprofit;
+    int num;
+    cout << " Enter the number: " << endl;
+    cin >> num;
+    int ans = addSum(num);
+    cout << "Your ans is:" << ans;
     return 0;
 }
