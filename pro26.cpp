7cf70e29a6f1644fcc36cd59679847e5005d986f
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-08-27 10:16:43 +0530

    dsa practice

---
+++ pro26.cpp
@@ -0,0 +1,29 @@
+//                        Leetcode question number--> 2706. Buy Two Chocolates
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int buyTwochco(vector<int>& prices , int money){
+    int ans = 0;
+    sort(prices.begin() , prices.end());
+    for (int i = 0; i < prices.size(); i++)
+    {
+        if((prices[0] + prices[1]) <= money ){
+           ans = money - (prices[0] + prices[1]);
+        }
+        else{
+            ans = money;
+        }
+    }
+    return ans;
+    
+}
+int main()
+{
+    vector<int>prices{6,7,8};
+    int money = 20;
+    int ans = buyTwochco(prices , money);
+
+    cout<<"After buying two choco you have " <<ans<<" money ";
+    return 0;
+}
