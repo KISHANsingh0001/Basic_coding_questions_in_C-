64cc011c34f293ecf7c3f9fd3b939f1c7228e8eb
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-08-29 00:34:40 +0530

    dsa practice

---
+++ pro29.cpp
@@ -0,0 +1,25 @@
+//    find the duplicate number in an array leetcode number 287
+#include<iostream>
+#include<vector>
+using namespace std;
+    int findDuplicate(vector<int>& nums) {
+       
+        // sort(nums.begin() , nums.end());
+        // for(int i = 0 ; i<nums.size()-1 ; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0] , nums[nums[0]]);
+        }
+        return nums[0];
+    }
+int main()
+{
+    vector<int>nums{1,3,4,2,2};
+    int ans = findDuplicate( nums);
+    cout<<"Duplicate number is : "<<ans;
+    return 0;
+}
