3dbada1dd3e8d005dc317e9e9217556b74ac180b
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-08-30 10:49:50 +0530

    dsa practice

---
+++ pro30.cpp
@@ -0,0 +1,31 @@
+//                        Binary search 
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarySearch(vector<int>& arr , int key){
+    int s = 0;
+    int e = arr.size()-1;
+    int mid = s + (e - s)/2;
+    while (s<e)
+    {
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] > key){
+            e = mid -1 ;
+        }
+        else{
+            s = mid + 1;
+        }
+        mid = s + (e - s)/2;
+    }
+    
+}
+int main()
+{
+    vector<int> arr{1,2,3,3,4,5,6,7,8,9,9};
+    int key = 6;
+    int ans = binarySearch(arr , key);
+    cout<<"key is present in the "<<ans<<" th index.";
+    return 0;
+}
---
+++ pro31.cpp
@@ -0,0 +1,40 @@
+//                      Leetcode problem number 35. Search Insert Position
+#include<iostream>
+#include<vector>
+using namespace std;
+int searchInsertposition(vector<int>& arr , int n , int key){
+    int low = 0; 
+    int high = n - 1;
+    while (low<=high)
+    {
+        int mid = low + (high - low)/2;
+        if(arr[mid] == key) return mid;
+
+        if(arr[mid] < key) low = mid + 1;
+        else{
+            high = mid - 1;
+        }
+    }
+    return low;
+    
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array:"<<endl;
+    cin>>n;
+    vector<int>arr;
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    int key;
+    cout<<"Enter the key what you want to find : ";
+    cin>>key;
+    int ans = searchInsertposition(arr , n , key);
+    cout<<ans;
+    
+    return 0;
+}
