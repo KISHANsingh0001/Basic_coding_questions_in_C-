5bd3b65624afbfa5a2762681a01e24fcd9aef5cd
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-09-02 14:30:01 +0530

    dsa practice

---
+++ pro33.cpp
@@ -0,0 +1,43 @@
+//          Rotate the array with kth position 
+#include<iostream>
+#include<vector>
+using namespace std;
+void rotateVector(vector<int>& temp , int k){
+         vector<int>nums;
+         for (int i = 0; i < temp.size(); i++)
+         {
+            nums[(i+k) % temp.size()] = temp[i];
+         }
+         temp = nums;
+         
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of the vector: ";
+    cin>>n;
+    int k;
+    cout<<"Enter the rotate point: ";
+    cin>>k;
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin>>ele;
+        temp.push_back(ele);
+    }
+     for (int i = 0; i < n; i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+    
+    rotateVector(temp , k);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+    
+    
+    return 0;
+}
