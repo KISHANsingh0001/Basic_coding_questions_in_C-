656129b20de8852ef45c212413a577e3a455d40c
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-09-13 19:16:45 +0530

    DSA practice

---
+++ pro40.cpp
@@ -0,0 +1,33 @@
+// Leet code problem number 507. Perfect Number
+// A perfect number is a positive integer that is equal to the sum of its positive divisors,
+//  excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+
+// Given an integer n, return true if n is a perfect number, otherwise return false.
+#include<iostream>
+#include<math.h>
+using namespace std;
+int isPerfectNumber(int n){
+    long long sum = 1;
+    for(long long int i = 2 ; i<=sqrt(n) ; i++){
+        if(n%i==0){
+            sum+=i;
+            sum+=n/i;
+        }
+    }
+    if(sum == n)return true;
+    else return false;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : "<<endl;
+    cin>>n;
+    if(isPerfectNumber(n)){
+        cout<<"Your number is perfect number.";
+    }
+    else{
+        cout<<"Your number is not a perfect number.";
+    }
+    return 0;
+}
