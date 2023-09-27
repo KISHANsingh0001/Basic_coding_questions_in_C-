f39269b5ca7bf303e3f44ec6134d2e0ea745bc8e
kishan singh thakur <kishasinghthakur27@gmail.com>
2023-09-27 12:23:51 +0530

    dsa practice

--- pro40_(LC)_507_Perfect_Number.cpp
+++ pro40_(LC)_507_Perfect_Number.cpp
@@ -1,4 +1,4 @@
-// Leet code problem number 507. Perfect Number
+// Leet code problem number (LC)_507_Perfect_Number
 // A perfect number is a positive integer that is equal to the sum of its positive divisors,
 //  excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
 
